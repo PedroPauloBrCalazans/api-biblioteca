e2f78994914c69ceda603183b1cbfc9de1bc99ca
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-10-04 23:18:52 -0300

    feat: Add validações de campos do mongoose.

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -8,9 +8,26 @@ const livroSchema = new mongoose.Schema(
       type: String,
       required: [true, "O título do livro é obrigatório"],
     },
-    editora: { type: String, required: [true, "A editora é obrigatório."] },
+    editora: {
+      type: String,
+      required: [true, "A editora é obrigatório."],
+      enum: {
+        values: ["Casa do código", "Alura"],
+        message: "A editora {VALUE} não é um valor permitido",
+      },
+    },
     preco: { type: Number },
-    paginas: { type: Number },
+    paginas: {
+      type: Number,
+      min: [
+        10,
+        "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}",
+      ],
+      max: [
+        5000,
+        "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}",
+      ],
+    },
     autor: autorSchema,
   },
   { versionKey: false }
