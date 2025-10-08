d9d213c49ee61d3c25d81a7a759a86715d6ff026
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-10-08 17:41:11 -0300

    feat: Add paginando a rota de listarLivros + ordenação

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,12 +1,31 @@
 import NaoEncontrado from "../erros/NaoEncontrado.js";
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 import { autor } from "../models/index.js";
 import { livro } from "../models/index.js";
 
 class LivroController {
   static async listarLivros(req, res, next) {
     try {
-      const listaLivros = await livro.find({});
-      res.status(200).json(listaLivros);
+      let { limite = 5, pagina = 1, ordenacao = "_id:-1" } = req.query;
+
+      let [campoOrdenacao, ordem] = ordenacao.split(":");
+
+      limite = parseInt(limite);
+      pagina = parseInt(pagina);
+      ordem = parseInt(ordem);
+
+      if (limite > 0 && pagina > 0) {
+        const listaLivros = await livro
+          .find()
+          // .sort({ titulo: 1 }) para ordenar pelo titulo em ordem alfabetica
+          .sort({ [campoOrdenacao]: ordem }) //por parametros na url
+          .skip((pagina - 1) * limite)
+          .limit(limite)
+          .exec();
+        res.status(200).json(listaLivros);
+      } else {
+        next(new RequisicaoIncorreta());
+      }
     } catch (erro) {
       next(erro);
     }
